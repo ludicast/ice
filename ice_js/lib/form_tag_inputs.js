31a771aedabff956805ffc9ef7e2d37de47198fe
Nate Kidwell <nate@ludicast.com>
2010-06-10 02:40:16 -0400

    added more label tag options

--- ice_js/lib/form_tag_inputs.js
+++ ice_js/lib/form_tag_inputs.js
@@ -1,3 +1,14 @@
-function label_tag(name) {
-    return "<label for=\"" + name + "\">" + name.charAt(0).toUpperCase() + name.substr(1) + "</label>"
+function humanize(name) {
+  match = name.match(/(.*)_id$/)
+  if (match) {
+      name = match[1]
+  }
+
+  return name.split('_').join(' ') 
+}
+
+function label_tag(name, opts) {
+    label = ((typeof opts == 'string') && opts) || humanize(name)
+    class_string = (opts && opts['class'] && "class=\"" + opts['class'] + "\" ") || ""
+    return "<label "+ class_string + "for=\"" + name + "\">" + label.charAt(0).toUpperCase() + label.substr(1) + "</label>"
 }
--- ice_js/spec/unit/spec.js
+++ ice_js/spec/unit/spec.js
@@ -105,19 +105,19 @@ describe "Form Builder Tags"
       tag = label_tag('name')
       tag.should.eql '<label for="name">Name</label>'
     end
-  //  it "assigns humanized default value" do
-   //   tag = "{% label_tag 'supervising_boss_id' %}"
-   //   tag.should parse_to('<label for="supervising_boss_id">Supervising boss</label>')
-  //  end
-  //  it "allows alternative value" do
-  /*    tag = "{% label_tag 'name', 'Your Name' %}"
-      tag.should parse_to('<label for="name">Your Name</label>')
+    it "assigns humanized default value"
+      tag = label_tag('supervising_boss_id')
+      tag.should.eql '<label for="supervising_boss_id">Supervising boss</label>'
     end
-    it "allows class to be assigned" do
-      tag = "{% label_tag 'name',class:'small_label' %}"
-      tag.should parse_to('<label class="small_label" for="name">Name</label>')
+    it "allows alternative value"
+      tag = label_tag('name', 'Your Name')
+      tag.should.eql '<label for="name">Your Name</label>'
     end
-    */
+    it "allows class to be assigned"
+      tag = label_tag('name', {'class':'small_label'})
+      tag.should.eql '<label class="small_label" for="name">Name</label>'
+    end
+
   end
 
 end
