df9350434033a88d8a34e6dd564bfd19a49c5db4
Nate Kidwell <nate@ludicast.com>
2011-06-29 08:49:40 -0400

    slight refactoring

--- lib/ice/eco_template/base.rb
+++ lib/ice/eco_template/base.rb
@@ -4,8 +4,7 @@ require 'v8'
 
 module Ice
   module EcoTemplate
-
-    def self.convert_template(template_text, vars = {})
+    def self.__convert_template(template_text)
       V8::C::Locker() do
         context = V8::Context.new
         context.eval(open "#{File.dirname(__FILE__)}/../../../js/lib/path-helper.js")
@@ -18,9 +17,15 @@ module Ice
         end
 
         context.eval CoffeeScript.compile(GeneratedHelpers.get_routes, :bare => true)
+        yield context
+
+      end
+    end
+
+    def self.convert_template(template_text, vars = {})
+      __convert_template(template_text) do |context|
         context.eval(Eco::Source.combined_contents)
         template = context["eco"]["compile"].call(template_text)
-
         template.call(vars.to_ice)
       end
     end
